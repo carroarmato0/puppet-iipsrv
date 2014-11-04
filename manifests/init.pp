8b298cc5769d5008a217525f81d7e4cf2e743178
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2014-11-04 15:00:34 +0100

    Add documentation for certain parameters and add some additional ones
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,11 @@
 class iipsrv (
-  $verbosity           = $iipsrv::params::verbosity,
   $logfile             = $iipsrv::params::logfile,
+  $verbosity           = $iipsrv::params::verbosity,
   $max_cache_size      = $iipsrv::params::max_image_cache_size,
   $quality             = $iipsrv::params::jpeg_quality,
   $max_cvt             = $iipsrv::params::max_cvt,
+  $max_layers          = $iipsrv::params::max_layers,
+  $filesystem_prefix   = $iipsrv::params::filesystem_prefix,
   $fcgid_idle_timeout  = $iipsrv::params::fcgid_idle_timeout,
   $fcgid_max_processes = $iipsrv::params::fcgid_max_processes_per_class,
   $script_alias        = $iipsrv::params::script_alias,
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,38 @@
 class iipsrv::params {
 
-  $verbosity                     = 5
-  $max_image_cache_size          = 10
-  $jpeg_quality                  = 50
-  $max_cvt                       = 3000
-  $fcgid_idle_timeout            = -1
-  $fcgid_max_processes_per_class = 1
-  $fcgi                          = '/usr/libexec/iipsrv/iipsrv.fcgi'
-  $script_alias                  = '/iipsrv'
+  # The log file the module will (attempt) to write to.
+  # If no value is given, no log will be written. Make sure the server process has write access to this directory.
+  # Paths with spaces in them may not work correctly.
   $logfile                       = $::operatingsystem ? {
     'centos' => '/var/log/httpd/iipsrv.log',
     'debian' => '/var/log/apache2/iipsrv.log',
   }
+  # The log file the module will (attempt) to write to. If no value is given, no log will be written.
+  # Make sure the server process has write access to this directory. Paths with spaces in them may not work correctly.
+  $verbosity                     = 1
+  # Max image cache size to be held in RAM in MB.
+  # This is a cache of the compressed JPEG image tiles requested by the client. The default is 10MB.
+  $max_image_cache_size          = 10
+  # The default JPEG quality factor for compression when the client does not specify one.
+  # The value should be between 1 (highest level of compression) and 100 (highest image quality). The default is 75.
+  $jpeg_quality                  = 75
+  # Limits the maximum image dimensions in pixels (the WID or HEI commands) allowable for dynamic JPEG export via the CVT command.
+  # This prevents huge requests from overloading the server. The default is 5000.
+  $max_cvt                       = 5000
+  # The maximum number of quality layers to decode for images that support progressive quality encoding, such as JPEG2000.
+  # Ignored for other file formats. If not set, half of the available quality layers will be decoded by default.
+  # If set to -1, all the available layers will be decoded by default.
+  $max_layers                    = ''
+  # This is a prefix automatically added by the server to the beginning of each file system path.
+  # This can be useful for security reasons to limit access to certain sub-directories.
+  # For example, with a prefix of “/home/images/” set on the server, a request by a client for “image.tif” will
+  # point to the path “/home/images/image.tif”.  Any reverse directory path component such as ../ is also filtered out. No default value.
+  $filesystem_prefix             = '/'
+
+  $fcgid_idle_timeout            = -1
+  $fcgid_max_processes_per_class = 1
+  $fcgi                          = '/usr/libexec/iipsrv/iipsrv.fcgi'
+  $script_alias                  = '/iipsrv'
   $conf                          = $::operatingsystem ? {
     'centos' => '/etc/httpd/conf.d/iipsrv.conf',
     'debian' => '/etc/apache2/conf.d/iipsrv.conf',
@@ -20,5 +41,6 @@ class iipsrv::params {
     'centos' => 'httpd',
     'debian' => 'apache2',
   }
+
 
 }
